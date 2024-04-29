41822391d64f1ff9604b74bf7ff452a61092f82a
bsedcode <bastiansedelmeyer@web.de>
2024-04-29 17:29:01 +0200

    Sportarten initialized calculation methods implemented

--- track/src/controller/Main.java
+++ track/src/controller/Main.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,5 +10,18 @@ public class Main {
 //		User eric = new User("Eric", "1.10.1996", 80, 1.83) ;
 //		System.out.println(eric.birthDate);
 //		System.out.println(eric.getBmi());
+/*		Scanner objekt = new Scanner(System.in);
+		System.out.println("Gib dein gewicht an");
+		double weight=objekt.nextDouble();
+		System.out.println(weight);
+		System.out.println("Wie groß bist du?");
+		double height=objekt.nextDouble();
+		*/
+		SportArten sport = new SportArten();
+		System.out.println(sport.biking(10, 30));
+		System.out.println(sport.walking(75, 5));
+		System.out.println(sport.football(90, 80));
+		System.out.println(sport.swimming(90, 80));
+		System.out.println(sport.handBall(60, 80));
 	}
 }
