9ed650e97d02162b1915a7410701ff815fc3fda2
bsedcode <bastiansedelmeyer@web.de>
2024-04-29 12:31:10 +0200

    User Class implemented

--- track/src/controller/Main.java
+++ track/src/controller/Main.java
@@ -3,6 +3,10 @@ package controller;
 public class Main {
 
 	public static void main(String[] args) {
-System.out.println("dei mudda");
+//		System.out.println("dei mudda");
+//
+//		User eric = new User("Eric", "1.10.1996", 80, 1.83) ;
+//		System.out.println(eric.birthDate);
+//		System.out.println(eric.getBmi());
 	}
 }
---
+++ track/src/controller/User.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class User {
+	private String name;
+	String birthDate;
+	private double weight;
+	private double height;
+	private boolean isMen;
+	private double bmi;
+	private double calories;
+	private double calcValue;
+	
+	public User (String valueName, String valuebirthDate, double valueWeight, double valueHeight, boolean valueGender) {
+		this.name= valueName;
+		this.birthDate= valuebirthDate;
+		this.weight= valueWeight;
+		this.height= valueHeight;
+		this.bmi = weight/(height*height);
+		this.isMen= true;
+	}
+	public double getBmi() {
+		return bmi;
+	}
+}
